6aea05765552047258ff7276668f2624c5956460
sagar sangwan <iamsagarsangwan@gmail.com>
2025-12-12 14:23:37 +0530

    added config file in auth service

---
+++ services/auth-service/app/core/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class settings:
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM: str = "HS256"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+    # Must be GREATER than frontend's 45 minutes
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 50
+    # Must be GREATER than frontend's 6 days
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+
+settings = settings()
